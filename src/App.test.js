31e8d3cdb2101fd6d483b80d660289aaadc18e7c
Singh <rhea.singh@capgemini.com>
2022-05-21 00:24:18 +0530

    added unit tests for App.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import PersonDetails from './components/personDetails';
+import Adapter from 'enzyme-adapter-react-16';
+import { shallow, configure } from 'enzyme';
 
-test('renders title', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Employee Income Details/i);
-  expect(linkElement).toBeInTheDocument();
+configure({adapter: new Adapter()});
+
+describe('App Component', () => {
+  const wrapper = shallow(<App />);
+
+  it('should render title', () => {
+    const element = wrapper.find('h1');
+    expect(element.text()).toEqual('Employee Income Details');
+  })
+
+  it('renders personDetails component', () => {
+    expect(wrapper.containsMatchingElement(<PersonDetails />)).toEqual(true);
+  });
 });
--- src/components/personDetails.test.js
+++ src/components/personDetails.test.js
@@ -1,14 +1,64 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
 import PersonDetails from './personDetails';
+import Adapter from 'enzyme-adapter-react-16';
+//import personsArray from '../assets/persons.json';
+import { shallow, configure } from 'enzyme';
 
-Enzyme.configure({ adapter: new Adapter() });
+configure({adapter: new Adapter()});
 
 describe('PersonDetails', () => {
-  it('should show the text', () => {
-  const personDetailsInstance = shallow(<PersonDetails />);
-  const element = personDetailsInstance.find('template');
-  expect(element).toBeInTheDocument();
+  const wrapper = shallow(<PersonDetails />);
+  const personsArray = 
+  [
+    {
+        "name": "Sarah Smith",
+        "age": 37
+    },
+    {
+        "name": "Lisa Crow",
+        "age": 35
+    },
+    {
+        "name": "Samuel Jackson",
+        "age": 53
+    },
+    {
+        "name": "Pamela Black",
+        "age": 47
+    },
+    {
+        "name": "Peter White",
+        "age": 36
+    }
+];
+
+  it('rendering data inside template based on provided input array', () => {
+  // There should be ONLY 1 template element
+  const template = wrapper.find('template');;
+  expect(template).toHaveLength(1);
+  // The dt should have as many elements as the personsArray array
+  const dt = template.find('dt');
+  expect(dt).toHaveLength(personsArray.length);
+  // The dd should have as many elements as the personsArray array
+  const dd = template.find('dd');
+  expect(dd).toHaveLength(personsArray.length);
+  // Loop through each row and check the content
+  // rows.forEach((tr, rowIndex) => {
+  //   const cells = tr.find('td');
+  //   expect(cells).toHaveLength(cols.length);
+  //   expect(cells.at(0).text()).toEqual(data[rowIndex].id);
+  //   expect(cells.at(1).text()).toEqual(data[rowIndex].name);
+  //   expect(cells.at(2).text()).toEqual(data[rowIndex].email);
  });
+
+ it("should update the component state", () => {
+  const form = wrapper.find('input');
+  // when
+  form.props().onChange({target: {
+     name: 'nameX',
+     value: 1234
+  }});
+  // then
+  expect(component.state('input')).toBeDefined();
+});
 });
