4467c1192fc243ea0ec0876357e0b5d64f69f000
Singh <rhea.singh@capgemini.com>
2022-05-20 16:36:34 +0530

    added minor css changes

--- src/components/personDetails.test.js
+++ src/components/personDetails.test.js
@@ -4,10 +4,11 @@ import Adapter from 'enzyme-adapter-react-16';
 import PersonDetails from './personDetails';
 
 Enzyme.configure({ adapter: new Adapter() });
+
 describe('PersonDetails', () => {
   it('should show the text', () => {
   const personDetailsInstance = shallow(<PersonDetails />);
-  const element = personDetailsInstance.find('div div');
-  expect(element.text()).toBe('This will be toggled');
+  const element = personDetailsInstance.find('template');
+  expect(element).toBeInTheDocument();
  });
 });
